197258f99f99e418fbaca0ddfaf3cbc04e6429b7
Shamal Karunarathne <shamal-c@silverglobe.com>
2017-11-06 16:21:32 +0800

    Completed 6. Routing
    Completed and Working stage of 6-Routing. Next step is 7. HTTP

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -12,6 +12,8 @@ var app_component_1 = require("./app.component");
 var hero_detail_component_1 = require("./hero-detail.component");
 var heroes_component_1 = require("./heroes.component");
 var hero_service_1 = require("./hero.service");
+var dashboard_component_1 = require("./dashboard.component");
+var app_routing_module_1 = require("./app-routing.module");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -21,10 +23,12 @@ AppModule = __decorate([
     core_1.NgModule({
         imports: [
             platform_browser_1.BrowserModule,
-            forms_1.FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+            forms_1.FormsModule,
+            app_routing_module_1.AppRoutingModule
         ],
         declarations: [
             app_component_1.AppComponent,
+            dashboard_component_1.DashboardComponent,
             hero_detail_component_1.HeroDetailComponent,
             heroes_component_1.HeroesComponent,
         ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
   ],
   providers: [HeroService],
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
 })
-
 export class AppModule { }
